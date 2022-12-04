a97716d1ecda1aa08daab5ce4bb3c39cb75b8fa7
alfkar <alfkar@student.chalmers.se>
2022-12-04 21:31:27 +0100

    day4 bad solution

---
+++ day4/day4part1.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int max(int v1, int v2){
+    if(v1>v2){
+        return v1;
+    }
+    return v2;
+}
+
+
+
+int splitter (string tickets){
+    int intTickets [4];
+    int stringValue = 0;
+    int pointer = 0;
+    for(int i = 0; i<tickets.size(); i++){
+    if(tickets[i] != '-' && tickets[i] != ','){
+            stringValue = stringValue*10 + tickets[i]%48;
+        }
+    if(tickets[i] == ',' || tickets[i] == '-' || i==tickets.size()-1){
+    intTickets[pointer] = stringValue;
+    //cout<<intTickets[pointer]<<"\n";
+    stringValue = 0;
+    pointer++;
+    }
+    }
+
+    if(intTickets[0] <= intTickets[2] && intTickets[2]<=intTickets[1] ){
+        return 1;
+    }
+    if(intTickets[1] >= intTickets[2] && intTickets[1] <= intTickets[3]){
+        return 1;
+    }
+
+    if(intTickets[2] <= intTickets[0] && intTickets[0] <= intTickets[3]){
+        return 1;
+    }
+   
+    return 0;
+
+}
+
+int main()
+{
+    vector<string> lines;
+    ifstream contents("input.txt");
+    for (string line; getline(contents, line);){
+        lines.push_back(line);
+    }
+    
+    int sum = 0;
+    for(int i = 0; i<lines.size(); i++){
+        sum += splitter(lines[i]);
+    }
+    cout<<sum;
+}
